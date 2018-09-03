0463fda71ae44bfb579971036cde99dde8a48ed9
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-03 22:06:33 +0200

    Initialize UI scheduler to avoid locking to V-sync

---
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/ui/MarbleMvvm.kt
@@ -0,0 +1,13 @@
+package cz.jhutarek.marble.arch.mvvm.ui
+
+import android.os.Looper.getMainLooper
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+object MarbleMvvm {
+
+    fun initialize() {
+        // TODO confirm that this is working: https://medium.com/@sweers/rxandroids-new-async-api-4ab5b3ad3e93
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { AndroidSchedulers.from(getMainLooper(), true) }
+    }
+}
