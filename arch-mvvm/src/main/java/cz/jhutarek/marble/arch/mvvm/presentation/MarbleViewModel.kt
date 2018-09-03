99e6d72d42b2835c22b83cf99f6b9017175fdb68
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-03 21:54:09 +0200

    Add states relay to view model

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
@@ -1,9 +1,13 @@
 package cz.jhutarek.marble.arch.mvvm.presentation
 
+import com.jakewharton.rxrelay2.BehaviorRelay
 import cz.jhutarek.marble.arch.mvvm.model.State
 import io.reactivex.Observable
 
-interface MarbleViewModel<S : State> {
+// TODO test
+abstract class MarbleViewModel<S : State> {
 
-    val states: Observable<S>
+    protected val statesRelay: BehaviorRelay<S> by lazy { BehaviorRelay.createDefault(defaultState) }
+    val states: Observable<S> = statesRelay.hide()
+    protected abstract val defaultState: S
 }
