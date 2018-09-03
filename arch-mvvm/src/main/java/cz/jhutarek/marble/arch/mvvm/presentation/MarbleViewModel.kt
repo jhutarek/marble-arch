61b02e4270d2187914595aa7cda1f75284721346
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-03 22:21:55 +0200

    Add TODO

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
@@ -7,7 +7,7 @@ import io.reactivex.Observable
 // TODO test
 abstract class MarbleViewModel<S : State> {
 
-    protected val statesRelay: BehaviorRelay<S> by lazy { BehaviorRelay.createDefault(defaultState) }
+    protected val statesRelay: BehaviorRelay<S> by lazy { BehaviorRelay.createDefault(defaultState) } // TODO is this 'lazy' trick OK?
     val states: Observable<S> = statesRelay.hide()
     protected abstract val defaultState: S
 }
