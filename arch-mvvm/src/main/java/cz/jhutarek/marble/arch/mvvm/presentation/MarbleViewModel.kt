0a9751d562dc18a7e4a0f40155019dfdaaa91cac
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-03 23:01:00 +0200

    Simplify view model updates

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/MarbleViewModel.kt
@@ -7,7 +7,11 @@ import io.reactivex.Observable
 // TODO test
 abstract class MarbleViewModel<S : State> {
 
-    protected val statesRelay: BehaviorRelay<S> by lazy { BehaviorRelay.createDefault(defaultState) } // TODO is this 'lazy' trick OK?
+    private val statesRelay: BehaviorRelay<S> by lazy { BehaviorRelay.createDefault(defaultState) } // TODO is this 'lazy' trick OK?
     val states: Observable<S> = statesRelay.hide()
     protected abstract val defaultState: S
+
+    protected fun update(updater: (S) -> S) {
+        statesRelay.accept(updater(statesRelay.value))
+    }
 }
