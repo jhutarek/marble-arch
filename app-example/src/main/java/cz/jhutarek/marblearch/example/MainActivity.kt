79ead151f674ea83f76efb4ffce447ff7a232cbe
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-03 21:05:28 +0200

    Rename app module

--- app-example/src/main/java/cz/jhutarek/marblearch/example/MainActivity.kt
+++ app-example/src/main/java/cz/jhutarek/marblearch/example/MainActivity.kt
@@ -1,12 +1,14 @@
-package cz.jhutarek.marblearch
+package cz.jhutarek.marblearch.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cz.jhutarek.marblearch.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
     }
 }
