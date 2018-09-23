d7b8a7f96f1017877f80567d98ac18d6d4bea7a9
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-23 17:55:04 +0200

    Fix main component

--- app-example/src/main/java/cz/jhutarek/marble/example/main/di/MainComponent.kt
+++ app-example/src/main/java/cz/jhutarek/marble/example/main/di/MainComponent.kt
@@ -2,13 +2,12 @@ package cz.jhutarek.marble.example.main.di
 
 import android.content.Context
 import cz.jhutarek.marble.example.counter.di.CounterComponent
-import cz.jhutarek.marble.example.counter.di.CounterModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [CounterModule::class])
+@Component
 interface MainComponent : CounterComponent {
 
     @Component.Builder
