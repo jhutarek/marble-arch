14d0094cebaf8eb756b2265db8c71fccc8d78b23
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-23 17:43:03 +0200

    Add context to main component

--- app-example/src/main/java/cz/jhutarek/marble/example/main/di/MainComponent.kt
+++ app-example/src/main/java/cz/jhutarek/marble/example/main/di/MainComponent.kt
@@ -1,9 +1,22 @@
 package cz.jhutarek.marble.example.main.di
 
+import android.content.Context
 import cz.jhutarek.marble.example.counter.di.CounterComponent
+import cz.jhutarek.marble.example.counter.di.CounterModule
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component
-interface MainComponent : CounterComponent
+@Component(modules = [CounterModule::class])
+interface MainComponent : CounterComponent {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun applicationContext(context: Context): Builder
+
+        fun build(): MainComponent
+    }
+}
--- app-example/src/main/java/cz/jhutarek/marble/example/main/system/MainApplication.kt
+++ app-example/src/main/java/cz/jhutarek/marble/example/main/system/MainApplication.kt
@@ -6,8 +6,15 @@ import com.squareup.leakcanary.LeakCanary
 import cz.jhutarek.marble.arch.mvvm.system.Mvvm
 import cz.jhutarek.marble.example.main.di.DaggerMainComponent
 import cz.jhutarek.marble.example.main.di.MainComponent
+import cz.jhutarek.marblearch.BuildConfig
+import timber.log.Timber
 
 class MainApplication : Application() {
+
+    private class TagPrefixDebugTree(private val tagPrefix: String) : Timber.DebugTree() {
+        override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) =
+                super.log(priority, tagPrefix + tag, message, throwable)
+    }
 
     companion object {
         fun getInjector(context: Context?) = (context?.applicationContext as? MainApplication)
@@ -23,8 +30,14 @@ class MainApplication : Application() {
         if (LeakCanary.isInAnalyzerProcess(this)) return
         LeakCanary.install(this)
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TagPrefixDebugTree("*MA: "))
+        }
+
         Mvvm.initialize()
 
-        component = DaggerMainComponent.create()
+        component = DaggerMainComponent.builder()
+                .applicationContext(applicationContext)
+                .build()
     }
 }
