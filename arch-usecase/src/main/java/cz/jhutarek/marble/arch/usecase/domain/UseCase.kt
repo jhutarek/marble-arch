017842132ec4a20dfb5c6607e115f174018605e1
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-26 14:39:24 +0200

    Make use case an interface

--- arch-usecase/src/main/java/cz/jhutarek/marble/arch/usecase/domain/UseCase.kt
+++ arch-usecase/src/main/java/cz/jhutarek/marble/arch/usecase/domain/UseCase.kt
@@ -1,20 +1,6 @@
 package cz.jhutarek.marble.arch.usecase.domain
 
-import io.reactivex.Completable
-import io.reactivex.Maybe
-import io.reactivex.Observable
-import io.reactivex.Single
-
-sealed class UseCase<in I, out O> {
-
-    abstract fun execute(input: I): O
-
-    abstract class ObservableUseCase<in I, O> : UseCase<I, Observable<O>>()
-
-    abstract class SingleUseCase<in I, O> : UseCase<I, Single<O>>()
-
-    abstract class MaybeUseCase<in I, O> : UseCase<I, Maybe<O>>()
-
-    abstract class CompletableUseCase<in I> : UseCase<I, Completable>()
+interface UseCase<in I, out O> {
+    fun execute(input: I): O
 }
 
