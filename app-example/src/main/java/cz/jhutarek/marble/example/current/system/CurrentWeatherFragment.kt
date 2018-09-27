91002604956372c7f5bd95612576c0febdccb3ba
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-28 01:20:06 +0200

    Show data in UI

--- app-example/src/main/java/cz/jhutarek/marble/example/current/system/CurrentWeatherFragment.kt
+++ app-example/src/main/java/cz/jhutarek/marble/example/current/system/CurrentWeatherFragment.kt
@@ -1,10 +1,14 @@
 package cz.jhutarek.marble.example.current.system
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import cz.jhutarek.marble.arch.mvvm.system.MarbleFragment
 import cz.jhutarek.marble.example.current.presentation.CurrentWeatherViewModel
 import cz.jhutarek.marble.example.main.system.MainApplication
 import cz.jhutarek.marblearch.R
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.current__current_weather_fragment.*
 
 class CurrentWeatherFragment : MarbleFragment<CurrentWeatherViewModel, CurrentWeatherViewModel.State>() {
 
@@ -15,5 +19,19 @@ class CurrentWeatherFragment : MarbleFragment<CurrentWeatherViewModel, CurrentWe
         super.onCreate(savedInstanceState)
 
         MainApplication.getInjector(context).inject(this)
+    }
+
+    override fun onBindStates(states: Observable<CurrentWeatherViewModel.State>) = states.subscribeForViews {
+        progressBar.visibility = if (it.loadingVisible) VISIBLE else GONE
+        emptyMessage.visibility = if (it.emptyVisible) VISIBLE else GONE
+        listOf(errorMessage, timestamp, location, temperature, pressure, description)
+                .forEach { view -> view.visibility = if (it.dataVisible) VISIBLE else GONE }
+        errorMessage.text = it.error
+        timestamp.text = it.timestamp
+        location.text = it.location
+        temperature.text = it.temperature
+        pressure.text = it.pressure
+        description.text = it.description
+        // TODO set wind, sunrise, sunset
     }
 }
