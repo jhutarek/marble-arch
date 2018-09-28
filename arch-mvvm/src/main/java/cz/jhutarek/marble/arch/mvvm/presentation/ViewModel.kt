846e26c738cad972c5c2fbe586ea8431102a3355
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-28 14:02:59 +0200

    Add logs

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/ViewModel.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/presentation/ViewModel.kt
@@ -3,12 +3,13 @@ package cz.jhutarek.marble.arch.mvvm.presentation
 import com.jakewharton.rxrelay2.BehaviorRelay
 import cz.jhutarek.marble.arch.mvvm.model.State
 import io.reactivex.Observable
+import timber.log.Timber
 
 // TODO test
 abstract class ViewModel<S : State>(defaultState: S) {
 
     protected val statesRelay: BehaviorRelay<S> = BehaviorRelay.createDefault(defaultState)
-    val states: Observable<S> = statesRelay.hide()
+    val states: Observable<S> = statesRelay.hide().doOnNext { Timber.d("State: $it") }
 
     protected fun BehaviorRelay<S>.accept(updater: (S) -> S) {
         accept(updater(value))
