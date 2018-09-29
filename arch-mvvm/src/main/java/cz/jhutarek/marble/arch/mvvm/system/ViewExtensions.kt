0309e21f810c730badf9b833b3575e0e1cd76145
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-09-29 22:26:22 +0200

    Do not set text if the value is not different

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/system/ViewExtensions.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/system/ViewExtensions.kt
@@ -5,7 +5,9 @@ import android.widget.EditText
 var EditText.textString: String?
     get() = text?.toString()
     set(value) {
-        val nullableSelectionStart = selectionStart.takeIf { it >= 0 }
-        setText(value)
-        nullableSelectionStart?.let { setSelection(it) }
+        if (value != text?.toString()) {
+            val nullableSelectionStart = selectionStart.takeIf { it >= 0 }
+            setText(value)
+            nullableSelectionStart?.let { setSelection(it) }
+        }
     }
