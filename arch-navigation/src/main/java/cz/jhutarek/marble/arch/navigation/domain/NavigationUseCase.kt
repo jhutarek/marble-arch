8d1583305f860067c99e91634edbb4fa0b303c52
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-11-29 14:29:20 +0100

    Add observe use case

--- arch-navigation/src/main/java/cz/jhutarek/marble/arch/navigation/domain/NavigationUseCase.kt
+++ arch-navigation/src/main/java/cz/jhutarek/marble/arch/navigation/domain/NavigationUseCase.kt
@@ -1,6 +1,7 @@
 package cz.jhutarek.marble.arch.navigation.domain
 
 import cz.jhutarek.marble.arch.usecase.domain.UseCase
+import io.reactivex.Observable
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,5 +10,10 @@ sealed class NavigationUseCase<in I, out O> : UseCase<I, O> {
     @Singleton
     class Navigate @Inject constructor(private val controller: NavigationController) : NavigationUseCase<Int, Unit>() {
         override fun invoke(input: Int) = controller.navigate(input)
+    }
+
+    @Singleton
+    class Observe @Inject constructor(private val controller: NavigationController) : NavigationUseCase<Unit, Observable<Int>>() {
+        override fun invoke(input: Unit): Observable<Int> = controller.observe()
     }
 }
--- arch-navigation/src/test/java/cz/jhutarek/marble/arch/navigation/domain/NavigationUseCaseTest.kt
+++ arch-navigation/src/test/java/cz/jhutarek/marble/arch/navigation/domain/NavigationUseCaseTest.kt
@@ -1,18 +1,43 @@
 package cz.jhutarek.marble.arch.navigation.domain
 
 import cz.jhutarek.marble.arch.test.infrastructure.InstancePerClassStringSpec
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.reactivex.Observable
 
 internal class NavigateUseCaseTest : InstancePerClassStringSpec({
     val controller = mockk<NavigationController>(relaxUnitFun = true)
     val destination = 123
 
-    val navigateTo = NavigationUseCase.Navigate(controller)
+    val navigate = NavigationUseCase.Navigate(controller)
 
-    "use case should execute navigate to on controller" {
-        navigateTo(destination)
+    "use case should execute navigate on controller" {
+        navigate(destination)
 
         verify { controller.navigate(destination) }
+    }
+})
+
+internal class ObserveUseCaseTest : InstancePerClassStringSpec({
+    val controller = mockk<NavigationController>()
+    val destination = 456
+
+    val observe = NavigationUseCase.Observe(controller)
+
+    "use case should execute observe on controller" {
+        every { controller.observe() } returns Observable.never()
+
+        observe(Unit)
+
+        verify { controller.observe() }
+    }
+
+    "use case should destination from controller" {
+        every { controller.observe() } returns Observable.just(destination)
+
+        observe(Unit)
+            .test()
+            .assertValue(destination)
     }
 })
