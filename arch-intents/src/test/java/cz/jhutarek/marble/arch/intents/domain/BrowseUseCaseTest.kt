b9575b9962ced60b5db5c7fb893a1aee2d0e439b
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-12-03 17:29:54 +0100

    Implement browse use case

---
+++ arch-intents/src/test/java/cz/jhutarek/marble/arch/intents/domain/BrowseUseCaseTest.kt
@@ -0,0 +1,46 @@
+package cz.jhutarek.marble.arch.intents.domain
+
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
+import cz.jhutarek.marble.arch.test.infrastructure.InstancePerClassStringSpec
+import io.kotlintest.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+
+internal class BrowseUseCaseTest : InstancePerClassStringSpec({
+    val context = mockk<Context>(relaxUnitFun = true)
+    val uri = Uri.parse("http://example.com")
+    val error = IllegalStateException()
+
+    val browse = BrowseUseCase(context)
+
+    "use case should try to start the activity with correct settings" {
+        val slot = slot<Intent>()
+        every { context.startActivity(capture(slot)) } returns Unit
+
+        browse(uri)
+            .test()
+
+        with(slot.captured) {
+            action shouldBe ACTION_VIEW
+            data shouldBe uri
+        }
+    }
+
+    "use case should return complete completable if activity is started" {
+        browse(uri)
+            .test()
+            .assertComplete()
+    }
+
+    "use case should return error completable if start activity throws an exception" {
+        every { context.startActivity(any())} throws error
+
+        browse(uri)
+            .test()
+            .assertError(error)
+    }
+})
