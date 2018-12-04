3adfc5bfbeedc4d529a35cc19607a2953b6b097f
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-12-04 15:36:21 +0100

    Add listeners to spec constructor

--- arch-test/src/main/java/cz/jhutarek/marble/arch/test/infrastructure/InstancePerClassStringSpec.kt
+++ arch-test/src/main/java/cz/jhutarek/marble/arch/test/infrastructure/InstancePerClassStringSpec.kt
@@ -1,8 +1,13 @@
 package cz.jhutarek.marble.arch.test.infrastructure
 
+import io.kotlintest.extensions.TestListener
 import io.kotlintest.specs.AbstractStringSpec
 import io.kotlintest.specs.StringSpec
 
-abstract class InstancePerClassStringSpec(body: AbstractStringSpec.() -> Unit = {}) : StringSpec(body) {
+abstract class InstancePerClassStringSpec(
+    spec: AbstractStringSpec.() -> Unit = {},
+    private vararg val listeners: TestListener
+) : StringSpec(spec) {
     final override fun isInstancePerTest() = true
+    final override fun listeners() = listeners.toList()
 }
