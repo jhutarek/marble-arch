84094529989423f5e4e9dadbe893d751553d2887
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-12-17 00:13:09 +0100

    Fix crash when text in edit text is nulled

--- arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/system/ViewExtensions.kt
+++ arch-mvvm/src/main/java/cz/jhutarek/marble/arch/mvvm/system/ViewExtensions.kt
@@ -7,7 +7,7 @@ var EditText.textString: String?
     get() = text?.toString()
     set(value) {
         if (value != text?.toString()) {
-            val nullableSelectionStart = selectionStart.takeIf { it >= 0 }
+            val nullableSelectionStart = selectionStart.takeIf { it >= 0 && it < value?.length ?: 0 }
             setText(value)
             nullableSelectionStart?.let { setSelection(it) }
         }
