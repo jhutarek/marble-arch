d5ad69b8c9a783ae9701f47bf63de353a8e0d0f0
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2019-01-30 21:37:48 +0100

    Add ability to inject services

--- arch-application/src/main/java/cz/jhutarek/marble/arch/application/system/MarbleApplication.kt
+++ arch-application/src/main/java/cz/jhutarek/marble/arch/application/system/MarbleApplication.kt
@@ -2,6 +2,7 @@ package cz.jhutarek.marble.arch.application.system
 
 import android.app.Activity
 import android.app.Application
+import android.app.Service
 import androidx.fragment.app.Fragment
 import com.squareup.leakcanary.LeakCanary
 import cz.jhutarek.marble.arch.application.di.MarbleApplicationComponent
@@ -10,14 +11,16 @@ import cz.jhutarek.marble.arch.log.infrastructure.logD
 import cz.jhutarek.marble.arch.mvvm.system.Mvvm
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import dagger.android.HasServiceInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
 // TODO test
-abstract class MarbleApplication : Application(), HasActivityInjector, HasSupportFragmentInjector {
+abstract class MarbleApplication : Application(), HasActivityInjector, HasSupportFragmentInjector, HasServiceInjector {
 
     @Inject internal lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
     @Inject internal lateinit var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>
+    @Inject internal lateinit var dispatchingServiceInjector: DispatchingAndroidInjector<Service>
 
     internal lateinit var component: MarbleApplicationComponent
 
@@ -40,6 +43,8 @@ abstract class MarbleApplication : Application(), HasActivityInjector, HasSuppor
     final override fun activityInjector() = dispatchingActivityInjector
 
     final override fun supportFragmentInjector() = dispatchingFragmentInjector
+
+    final override fun serviceInjector() = dispatchingServiceInjector
 
     protected open fun onInitialize() {}
 
